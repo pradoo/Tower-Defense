8a54284bd3680a4214436529fa2c9622d2789b4b
Spencer <spencerreynoso@live.com>
2013-10-21 14:53:06 -0500

    added strings

--- gen/com/example/towerdefense/R.java
+++ gen/com/example/towerdefense/R.java
@@ -40,6 +40,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int load_game=0x7f050004;
+        public static final int new_game=0x7f050005;
         public static final int title_activity_tower_game=0x7f050003;
     }
     public static final class style {
