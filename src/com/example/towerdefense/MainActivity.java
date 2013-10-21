f08e352f3a51c98ad92bcfdc59c03100de34ede3
Spencer Reynoso <spencerreynoso@live.com>
2013-10-21 12:02:37 -0500

    made simple intent for next activity

--- src/com/example/towerdefense/MainActivity.java
+++ src/com/example/towerdefense/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.towerdefense;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	private static int[] ids = {R.id.new_game, R.id.keepgoing};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//jfsl;jfskljdfsljfdsfsdfs
-		//fjsdlfjlsljfs
+
+		View[] buttons = new View[ids.length];
+		for(int i = 0; i < buttons.length; ++i){
+			buttons[i] = findViewById(ids[i]);
+			buttons[i].setOnClickListener(this);
+		}
 	}
 
 	@Override
@@ -19,6 +27,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+			case R.id.new_game:
+				Intent i = new Intent(this, TowerGame.class);
+				startActivity(i);
+				break;
+			case R.id.keepgoing:
+				Intent f = new Intent(this, TowerGame.class);
+				startActivity(f);
+				break;
+		}
 	}
 
 }
