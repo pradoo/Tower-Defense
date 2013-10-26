3f769bc34e3ef0dadda861725af6a0ad22864917
Spencer Reynoso <spencerreynoso@live.com>
2013-10-25 21:27:10 -0500

    got the background to be green and made the app fullscreen

--- gen/com/example/towerdefense/R.java
+++ gen/com/example/towerdefense/R.java
@@ -10,6 +10,9 @@ package com.example.towerdefense;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int dark_green=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,18 +27,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int board=0x7f080002;
-        public static final int keepgoing=0x7f080001;
-        public static final int new_game=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int board=0x7f090002;
+        public static final int keepgoing=0x7f090001;
+        public static final int new_game=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_tower_game=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int tower_game=0x7f070001;
+        public static final int main=0x7f080000;
+        public static final int tower_game=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -67,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/towerdefense/BoardView.java
+++ src/com/example/towerdefense/BoardView.java
@@ -1,19 +1,44 @@
 package com.example.towerdefense;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class BoardView extends View{
 
 	private TowerGameLogic mGame;
+	private Paint mPaint;
 	
 	public BoardView(Context context) {
 		super(context);
+		initialize();
 		// TODO Auto-generated constructor stub
+	}
+	
+	public BoardView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		initialize();
+	}
+	
+	public BoardView(Context context, AttributeSet attrs) {
+		super(context, attrs);	       
+		initialize();
+	}
+	
+	public void initialize() {   
+		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	}
 	
 	public void setGame(TowerGameLogic game) {
 		mGame = game;
 	}	
+	
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		super.onDraw(canvas);	
+	}
 
 }
