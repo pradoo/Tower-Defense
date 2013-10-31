7da95e4ffbf2747fd29bc53e9199817703c7ff3b
Soren13 <austin.liu@att.net>
2013-10-31 17:36:59 -0500

    Did a lot with the Tower.java file. Look over it and see if there's any suggestions/modifications you guys have.

--- src/com/example/towerdefense/Tower.java
+++ src/com/example/towerdefense/Tower.java
@@ -1,12 +1,19 @@
 package com.example.towerdefense;
 
+import java.util.ArrayList;
+
 public class Tower {
 
-	//Not sure if this needs to stay int
-	//Also not sure if 'final'
-	final int RANGE;
-	int damage;
-	
+	private int[] range;
+	//Range can be flattened into a single int
+	//IF we want xRange == yRange
+
+	private int[] position;
+	private int damage;
+	private TowerGameLogic logic;
+	private ArrayList<EnemyCircle> enemiesInRange;
+
+	// TODO
 	//PriorityQueue for enemies goes here
 	/*
 	 * Priority based on enemy health, unless we aren't allowing
@@ -14,13 +21,108 @@ public class Tower {
 	 * the end?
 	 * 
 	 */
-	
-	public Tower(int starting_range) {
-		RANGE = starting_range;
-		damage = 10;//subject to change based on enemies
+
+	/**
+	 * Constructor for the Towers. Make sure to send me the TowerGameLogic object.
+	 * Otherwise, I won't know how to access the enemies :)
+	 * @param gameLogic required to access the list of enemies
+	 */
+	public Tower(TowerGameLogic gameLogic) {
+		range = new int[2];	
+		position = new int[2];
+		logic = gameLogic;
+
+		//The following are subject to change as needed
+		damage = 2;
+		range[0] = 1;
+		range[1] = 1;
+	}
+
+	public int[] getRange() {
+		return range;
+	}
+
+	/**
+	 * Used for upgrading the tower if we get to that point
+	 * @param newX the new X range distance
+	 * @param newY the new Y range distance
+	 */
+	public void setRange(int newX, int newY) {
+		range[0] = newX;
+		range[1] = newY;
+	}
+
+	/**
+	 * Returns how much damage the tower will inflict on the enemy
+	 * @return The amount of damage the tower will do
+	 */
+	public int getDamage() {
+		return damage;
+	}
+
+	/**
+	 * Sets a new damage amount for the tower
+	 * @param newDamage the new damage amount
+	 */
+	public void setDamage(int newDamage) {
+		damage = newDamage;
+	}
+
+	//Not sure if this is necessary, but I made it anyway
+	public int[] getPosition() {
+		return position;
+	}
+
+	/**
+	 * Call this when the tower is being moved from it's previous position.
+	 * @param x Tower's new X coordinate
+	 * @param y Tower's new Y coordinate
+	 */
+	public void setPosition(int x, int y) {
+		position[0] = x;
+		position[1] = y;
+	}
+
+	private boolean inRange(EnemyCircle enemy) {
+		//Determine if the enemy is left or right of tower
+		boolean inX = (enemy.getXpos() < position[0]) ?
+				position[0] - range[0] <= enemy.getXpos() :
+				position[0] + range[0] >= enemy.getXpos() ;
+
+		//Determine if the enemy is above or below tower
+		boolean inY = (enemy.getYpos() < position[1]) ?
+				position[1] - range[1] <= enemy.getYpos() :
+				position[1] + range[1] >= enemy.getYpos() ;
+
+		return inX && inY;
+	}
+
+	/**
+	 * This method finds all enemies within firing range of the tower.
+	 */
+	public void findEnemiesInRange() {
+		enemiesInRange.clear();
+		ArrayList<EnemyCircle> temp = logic.getEnemies();
+
+		for(EnemyCircle e: temp)
+			if(inRange(e))
+				enemiesInRange.add(e);
 	}
 	
-	public void findEnemiesInRange() {
-		//Help fill this in once enemies are implemented
+	//Is it possible to combine findEnemiesInRange() and attackEnemies()?
+	//I feel like these two methods may take longer than we have to allow...
+	
+	/**
+	 * This method will attack the enemies within the tower's firing range.
+	 * This is done by using the EnemyCircle.setHealth() method.
+	 */
+	public void attackEnemies() {
+		EnemyCircle temp;
+		
+		//Afraid to use a for-each loop because half the time it doesn't work
+		for(int i = 0; i < enemiesInRange.size(); ++i) {
+			temp = enemiesInRange.get(i);
+			temp.setHealth(temp.getHealth()-damage);
+		}
 	}
 }
