6df624ff8b9d54e26aa81bdd8af03fdbd7e7d17f
pradoo <prado@utexas.edu>
2013-11-30 22:16:20 -0600

    Added radial gradient and ANTI_ALIAS_FLAG to paint for enemies

--- src/com/example/towerdefense/EnemyCircle.java
+++ src/com/example/towerdefense/EnemyCircle.java
@@ -5,14 +5,15 @@ import java.util.LinkedList;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RadialGradient;
 import android.graphics.Rect;
+import android.graphics.Shader;
 public class EnemyCircle extends AbsEnemy{
 
 
 	public EnemyCircle(int x, int y, int r, LinkedList<int []> i, TowerGameLogic g){
 		super(x, y, r, i, g, 0.75);
-		mPaint = new Paint();
-
+		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		speed = 2;
 		setHealth(700);
 		gold  = 25;
@@ -23,6 +24,9 @@ public class EnemyCircle extends AbsEnemy{
 	public void draw(Canvas canvas) {
 		mPaint.setColor(Color.BLACK);        
 		mPaint.setStrokeWidth(5);
+		RadialGradient rg;
+		rg = new RadialGradient(position[0], position[1] - 4, radius, Color.WHITE, mPaint.getColor(), Shader.TileMode.CLAMP);
+		mPaint.setShader(rg);
 		int t = (int) (radius*0.75);
 		bounds = new Rect(position[0]-t, position[1]-t,position[0]+t, position[1]+t);
 		canvas.drawCircle(position[0], position[1], radius, mPaint);
