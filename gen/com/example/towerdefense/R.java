10a86094aeddfaef1f33a0388479202b9bb1d209
pradoo <prado@utexas.edu>
2013-12-02 08:57:10 -0600

    added text view for home screen title

--- gen/com/example/towerdefense/R.java
+++ gen/com/example/towerdefense/R.java
@@ -31,18 +31,19 @@ public final class R {
         public static final int sound=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0009;
-        public static final int board=0x7f0a0007;
-        public static final int gold=0x7f0a0005;
+        public static final int action_settings=0x7f0a000a;
+        public static final int board=0x7f0a0008;
+        public static final int gold=0x7f0a0006;
         public static final int level1=0x7f0a0000;
         public static final int level2=0x7f0a0001;
         public static final int level3=0x7f0a0002;
-        public static final int level_num=0x7f0a0004;
-        public static final int lives=0x7f0a0006;
-        public static final int new_game=0x7f0a000a;
-        public static final int play=0x7f0a0003;
-        public static final int sound=0x7f0a000b;
-        public static final int towerinfo=0x7f0a0008;
+        public static final int level_num=0x7f0a0005;
+        public static final int lives=0x7f0a0007;
+        public static final int new_game=0x7f0a000b;
+        public static final int play=0x7f0a0004;
+        public static final int sound=0x7f0a000c;
+        public static final int textView1=0x7f0a0003;
+        public static final int towerinfo=0x7f0a0009;
     }
     public static final class layout {
         public static final int activity_level_pick=0x7f030000;
